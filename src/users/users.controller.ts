93147d369460535a5cd8068239012af25720745f
KamilFil <kamilfprog@gmail.com>
2023-02-20 20:49:52 +0100

    Create method user create - and create validation exist username and email

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {Response} from "express";
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  register(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.register(createUserDto);
+  create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    return this.usersService.create(createUserDto, res);
   }
 
   @Get()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import {Response} from "express";
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto, res: Response) {
+
+    const checkUsername = await User.findOneBy({username: createUserDto.username})
+    const checkEmail = await User.findOneBy({email: createUserDto.email})
+
+    if (checkUsername) {
+      throw new BadRequestException("Users exist, change your username")
+    }
+
+    if (checkEmail) {
+      throw new BadRequestException("Email exist, change your email")
+    }
+
+    const createUser = new User()
+
+    createUser.username = createUserDto.username;
+    createUser.email = createUserDto.email;
+    createUser.password = createUserDto.password; // Will added hash password.
+
+    await createUser.save()
+
+    return res.json({isSucces:true})
+
   }
 
-  findAll() {
+  getAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  getOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
