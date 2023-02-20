d415feac9910b2c8ea7a0edd8c13f43e2428113f
KamilFil <kamilfprog@gmail.com>
2023-02-20 20:25:40 +0100

    generate users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {DbConfig} from "./config/database.config";
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(DbConfig)],
+  imports: [TypeOrmModule.forRoot(DbConfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
