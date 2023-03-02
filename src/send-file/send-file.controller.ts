cffa3480050acaa36e05f0034a71c18a8473aa29
KamilFil <kamilfprog@gmail.com>
2023-03-02 20:02:49 +0100

    added route upload in controller, create multerconfig img files

--- src/send-file/send-file.controller.ts
+++ src/send-file/send-file.controller.ts
@@ -1,34 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { SendFileService } from './send-file.service';
-import { CreateSendFileDto } from './dto/create-send-file.dto';
-import { UpdateSendFileDto } from './dto/update-send-file.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerConfig } from '../config/multer.config';
 
 @Controller('send-file')
 export class SendFileController {
   constructor(private readonly sendFileService: SendFileService) {}
 
-  @Post()
-  create(@Body() createSendFileDto: CreateSendFileDto) {
-    return this.sendFileService.create(createSendFileDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.sendFileService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.sendFileService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSendFileDto: UpdateSendFileDto) {
-    return this.sendFileService.update(+id, updateSendFileDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.sendFileService.remove(+id);
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', multerConfig))
+  async addFile(@UploadedFile() file: Express.Multer.File) {
+    return `File ${file.originalname} update`;
   }
 }
--- src/send-file/send-file.service.ts
+++ src/send-file/send-file.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSendFileDto } from './dto/create-send-file.dto';
-import { UpdateSendFileDto } from './dto/update-send-file.dto';
+import * as fs from 'fs';
 
 @Injectable()
 export class SendFileService {
-  create(createSendFileDto: CreateSendFileDto) {
-    return 'This action adds a new sendFile';
-  }
+  async addFile(file: Express.Multer.File) {
 
-  findAll() {
-    return `This action returns all sendFile`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} sendFile`;
-  }
-
-  update(id: number, updateSendFileDto: UpdateSendFileDto) {
-    return `This action updates a #${id} sendFile`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} sendFile`;
   }
 }
