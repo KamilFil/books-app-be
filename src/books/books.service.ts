813cbb55a72c2160370f88466c624d44b063fa49
KamilFil <kamilfprog@gmail.com>
2023-03-08 19:15:55 +0100

    Added types for create books, added entities for books, create method service for books (create, update, getAll, findOne, remove), added entities to config database

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './entities/book.entity';
 
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+  async create(createBookDto: CreateBookDto) {
+    const findBook = await Book.findOne({
+      where: { name: createBookDto.name },
+    });
+
+    if (findBook) {
+      throw new BadRequestException("Book's exist, change name");
+    }
+
+    const book = new Book();
+    book.name = createBookDto.name;
+    book.author = createBookDto.author;
+    book.img = createBookDto.img;
+    book.description = createBookDto.description;
+
+    await book.save();
+
+    return `${book.name}`;
   }
 
-  findAll() {
-    return `This action returns all books`;
+  async findAll() {
+    return await Book.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
+  async findOne(id: string) {
+    return await Book.findOneBy({ id: id });
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+  async update(id: string, updateBookDto: UpdateBookDto) {
+    const findBook = await Book.findOneBy({ id: id });
+
+    if (!findBook) {
+      throw new BadRequestException("Book's not exist");
+    }
+
+    await Book.update(id, updateBookDto);
+
+    return `Update ${findBook.name} books`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} book`;
+  async remove(id: string) {
+    const findBook = await Book.findOneBy({ id: id });
+
+    if (!findBook) {
+      throw new BadRequestException("Book's not exist");
+    }
+
+    await Book.delete(id);
+
+    return `Remove ${findBook.name} books`;
   }
 }
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,18 @@
-export class Book {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Book extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'simple-array', default: null })
+  author: string[];
+
+  @Column({ type: 'simple-array', default: null })
+  img: string[];
+
+  @Column()
+  description: string;
+}
